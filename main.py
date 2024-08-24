9e9456c0fbc9512f4c49dedf9d55cfa851fed0e5
Kelvin <ndtdat21@clc.fitus.edu.vn>
2024-08-24 16:40:55 +0700

    add background

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
 
+# set_background('./.png')
 
 #Dashboard
 st.title('Object Detector')
@@ -9,4 +10,3 @@ upload = st.file_uploader(label='Upload image here', type=['png','jpg', 'jpeg'])
 
 if upload:
     img = Image.open(upload)
-    
